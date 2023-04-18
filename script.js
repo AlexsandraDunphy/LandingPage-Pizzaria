ec99880f7e1a981c094b5a26b9003050197377a1
Alexsandra Lima <alexsandracontact@gmail.com>
2023-04-18 13:42:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
+
+let show = true;
+const menuContent = document.querySelector('.content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () => {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    menuContent.classList.toggle('on', show);
+    show = !show;
+})
